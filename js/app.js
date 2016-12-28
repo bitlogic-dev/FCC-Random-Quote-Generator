430ade3544d2cdda1839c55f48059377938cfdc4
Haig Hagopian <neofusionzero@gmail.com>
2016-12-27 16:15:29 -0800

    Fixed Twitter JS Factory

--- js/app.js
+++ js/app.js
@@ -1,25 +1,10 @@
 $(document).ready(function() {
-  //load twitter javascript factory
-  window.twttr = (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0],
-      t = window.twttr || {};
-    if (d.getElementById(id)) return t;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
-    t._e = [];
-    t.ready = function(f) {
-      t._e.push(f);
-    };
-    return t;
-  }(document, "script", "twitter-wjs"));
   //load initial quote and build tweet button
-  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
+  $.getJSON('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1', function(json) {
     var quote = json.shift();
-    var html = "";
-    html += "<p>" + quote.content + "</p><br>";
-    html += "<p>- " + quote.title + "</p>";
+    var html = '';
+    html += '<p>' + quote.content + '</p><br>';
+    html += '<p>- ' + quote.title + '</p>';
     $("#quoteBox").html(html);
       var twtterContainer = $('<a></a>')
     .addClass('twitter-share-button')
@@ -31,14 +16,14 @@ $(document).ready(function() {
   twttr.widgets.load();
   });
   //reload quote and tweet button when new quote is clicked
-  $("#randomQuote").on("click", function() {
-    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
+  $('#randomQuote').on('click', function() {
+    $.getJSON('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1', function(json) {
       var quote = json.shift();
-      var html = "";
-      html += "<p>" + quote.content + "</p><br>";
-      html += "<p>- " + quote.title + "</p>";
-      $("#quoteBox").html(html);
-      customText = $("#quoteBox").text();
+      var html = '';
+      html += '<p>' + quote.content + '</p><br>';
+      html += '<p>- ' + quote.title + '</p>';
+      $('#quoteBox').html(html);
+      customText = $('#quoteBox').text();
       $('#twtterContainer iframe').remove();
       var twtterContainer = $('<a></a>')
         .addClass('twitter-share-button')
